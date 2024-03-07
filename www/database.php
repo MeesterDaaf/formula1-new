3573b7612ef756350ebe1498203f922cabcb1c0b
David van Oudheusden <doudheusden@novacollege.nl>
2024-03-07 13:30:36 +0100

    added few changes

--- www/database.php
+++ www/database.php
@@ -2,6 +2,14 @@
 
 //database connection  
 
+//database connection  
+
 $dbname = "formula1"; // Database name 
 
-$conn = mysqli_connect("localhost", "root", "password", $dbname);
+$conn = mysqli_connect("mariadb", "root", "password", $dbname);
+
+
+// If you are using XAMPP, the password is empty by default, so you can leave it empty. See below:
+// $conn = mysqli_connect("localhost", "root", "", $dbname);
+
+
